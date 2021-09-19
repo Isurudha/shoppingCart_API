c513f8c35312d9e6eb34c05bd8e62b39c017652c
RavinduTharaka <44726946+RavinduTharaka@users.noreply.github.com>
2021-09-19 22:53:24 +0530

    post method for auth

--- routes/authentication.js
+++ routes/authentication.js
@@ -6,6 +6,28 @@ const jwtoken = require("jsonwebtoken");
 const User = require("../models/user");
 const API_KEY = process.env.API_KEY;
 
-
+router.post("/", async (req, res) => {
+    let user = await User.findOne({ email: req.body.email });
+    console.log(req.body.email, req.body.password);
+    if (!user)
+      return res
+        .status(400)
+        .send({ message: "Invalid email / pw provided for authentication" });
+  
+    let pwValid = await bcrypt.compare(req.body.password, user.password);
+    if (!pwValid)
+      return res
+        .status(400)
+        .send({ message: "Invalid email / pw provided for authentication" });
+  
+    let token = jwt.sign(
+      { id: user._id, email: user.email, isAdmin: user.isAdmin },
+      API_KEY
+    );
+    return res
+      .status(200)
+      .send({ token: token, isAdmin: user.isAdmin, id: user._id });
+  });
+  module.exports = router;
 
   
